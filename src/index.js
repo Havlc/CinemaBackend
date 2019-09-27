226801274219e4c6ee53771394903231db2b3133
Konrad C <konrad1988@interia.pl>
2019-09-27 12:28:59 +0200

    add routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const routes = require('./routes/movie');
 
 app.use(cors());
 
 require("./startup/connectDB")();
+
+// Routes
+app.use('/', routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
