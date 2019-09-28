667b26d3e55450c246ad12747f56b13a43a2cda9
Konrad C <konrad1988@interia.pl>
2019-09-28 16:22:06 +0200

    add routes get movies & get movies/:id

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
-const Repertoire = require('../models/Repertoire');
+const { Repertoire } = require('../models/Repertoire');
+
+// RESTful Routes
+
+router.get("/", (req, res)=>{
+res.redirect("api/movies");
+})
+
+router.get("/api/movies", async (req,res)=>{
+  const daysOfTheWeek = await Repertoire.find()
+  res.send(daysOfTheWeek);
+});
+
+
+router.get("/api/movies/:id", async (req, res) => {
+    const dayOfTheWeek = await Repertoire.findById(req.params.id);
+    res.send(dayOfTheWeek);
+});
 /*
-router.get("/", (req, res) => {
-    Repertoire.find({title: {}}, (err, allMovies)=>{
-        if(err){
-            console.log(err);
-        } else {
-           res.send(allMovies);
-        }
-     });
-});
+router.put("/api/movies/:id", async (req, res) => {
+  const days = await Repertoire.findByIdAndUpdate(req.params.id, {
+    Id: req.body.Id, 
+    date: req.body.date,
+    movies: req.body.movies
+  });
+  
+  if (!days) return res.status(404).send('The repertoire with the given ID was not found.');
+  
+  res.send(days);
+});*/
 
-router.get("/:id", async (req, res) => {
-    const movie = await Movie.findById(req.params.id);
-    res.send(movie);
-});
-
-router.put("/:id", async (req, res) => {
-
-});
-*/
 module.exports = router;
